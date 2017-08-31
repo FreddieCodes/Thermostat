68e8effda79fa1f2cba7f18ad40de2794412438f
Frederick Ofori <FrederickCodes@gmail.com>
2017-08-31 15:56:21 +0100

    Power Saving (it works)

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,7 +2,7 @@ describe("Thermostat", function() {
 beforeEach(function(){
   thermostat = new Thermostat()
 });
- 
+
   it("should have a default temperature of 20 degrees", function(){
     expect(thermostat.temp).toEqual(20)
   })
@@ -17,5 +17,28 @@ beforeEach(function(){
     expect(thermostat.temp).toEqual(19)
   })
 
+  it("should have a minimum temperature of 10 degrees", function(){
+    thermostat.temp = 10
+    expect(thermostat.down()).toEqual('Could not lower temperature')
+  });
+
+  describe("Power mode saving", function() {
+    it("ON - should have a maximum temperature of 25 degrees ", function (){
+      var message = "Can not exceed 25 degrees in this mode."
+      thermostat.temp = 25
+      expect(function(){
+        thermostat.up()
+      }).toThrowError(message)
+    });
+
+    it("OFF - should have a maximum temperature of 32 degrees", function (){
+      var message = "Can not exceed 32 degrees in this mode."
+      thermostat.temp = 32
+      thermostat.powerSavingButton()
+      expect(function(){
+        thermostat.up()
+      }).toThrowError(message)
+    });
+  });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,11 +3,27 @@ function Thermostat () {
 };
 
 Thermostat.prototype.temp = 20
+Thermostat.prototype.powerSaving = true
 
 Thermostat.prototype.up = function() {
+  var message = "Can not exceed 25 degrees in this mode."
+  var message2 = "Can not exceed 32 degrees in this mode."
+  if (this.temp >= 25 && this.powerSaving === true) throw new Error(message);
+  else if (this.temp >= 32 && this.powerSaving === false) throw new Error(message2);
   this.temp += 1
 }
 
 Thermostat.prototype.down = function() {
-  this.temp --
+  if (this.temp <= 10) {
+    return 'Could not lower temperature';
+  }
+    this.temp --
+}
+
+Thermostat.prototype.powerSavingButton = function() {
+  if (this.powerSaving === true) {
+    this.powerSaving = false
+  } else {
+    this.powerSaving = true
+  }
 }
